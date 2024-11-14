17debd897f2f91584d6f3884cbbc7fb02fa1a66b
Elvan Hidayat <elvanhd@gmail.com>
2024-11-14 22:05:57 +0700

    Add postalCode field with pattern validation to Customer model
    - Add `postalCode` field to `Customer` with regex pattern validation
    - Update `customer-form.html` to include `postalCode` input field
    - Display `postalCode` value in `customer-confirmation.html`
    - Show error message if `postalCode` validation fails

--- src/main/java/com/github/elvan/mvcvalidationdemo/Customer.java
+++ src/main/java/com/github/elvan/mvcvalidationdemo/Customer.java
@@ -3,6 +3,7 @@ package com.github.elvan.mvcvalidationdemo;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -16,6 +17,17 @@ public class Customer {
   @Min(value = 0, message = "must be greater than or equal to zero")
   @Max(value = 10, message = "must be less than or equal to 10")
   private int freePasses;
+
+  @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+  private String postalCode;
+
+  public String getPostalCode() {
+    return postalCode;
+  }
+
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
+  }
 
   public int getFreePasses() {
     return freePasses;
